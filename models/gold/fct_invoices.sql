acc59a5b5b1879a12ed65e2756a5939331694201
Teun Jorink <teun.jorink@digital-power.com>
2025-09-26 10:42:34 +0200

    fact tabel toegevoegd

---
+++ models/gold/fct_invoices.sql
@@ -0,0 +1,9 @@
+select
+orders.*,
+customer.name, as customer_name
+customer.acctbal as account_balance
+
+from {{ ref('silver_orders') }} as orders
+
+left join {{ ref('silver_customer') }} as customer
+on orders.custkey = customer.custkey
