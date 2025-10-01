256f5592dff19f116aaf9b8e703748b41bae8e13
Teun Jorink <teun.jorink@digital-power.com>
2025-10-01 17:20:45 +0200

    fixed error

--- macros/recent_days_filter.sql
+++ macros/recent_days_filter.sql
@@ -1,5 +1,6 @@
-{% macro recent_days_filter(date_field, n_days=90) %}
+{% macro recent_days_filter(date_field, days_back=90) %}
 
     {{ date_field }} >= current_date - interval '{{ days_back }} day'
 
 {% endmacro %}
+git
